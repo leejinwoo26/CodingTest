f8b6261d760b36622bf20a9c5ea711171cb1c5d5
이진우 <142082239+leejinwoo26@users.noreply.github.com>
2024-10-18 17:29:58 +0900

    [level 2] Title: 타겟 넘버, Time: 96.63 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+    int answer = 0;
+
+    void get_target_number(vector<int> numbers, int target, int sum, int index) {
+        //종료 조건
+        if (index == numbers.size()) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        }
+        get_target_number(numbers, target, sum + numbers[index], index + 1);
+        get_target_number(numbers, target, sum - numbers[index], index + 1);
+    }
+
+    int solution(vector<int> numbers, int target) 
+    {
+        get_target_number(numbers, target, 0, 0);
+        return answer;
+    }
+
