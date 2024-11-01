524683e65ca8e4deccb2e1854b9f286e07122582
이진우 <142082239+leejinwoo26@users.noreply.github.com>
2024-11-02 01:14:51 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -3,29 +3,13 @@
 
 using namespace std;
 
-bool check(char a, char b)
-{
-    if (a == b)
-        return true;
-    else
-        return false;
-}
-
 int solution(string my_string, string is_prefix) {
-    int answer = 0;
-    bool isPrefix = true;
+    int answer = 1;
     if (is_prefix.size() > my_string.size())
         return 0;
-
-    for (int i = 0; i < is_prefix.size(); i++)
-    {
-        if (!check(is_prefix[i], my_string[i]))
-        {
-            isPrefix = false;
-        }
+    for (int i = 0; i < is_prefix.size(); i++){
+        if (is_prefix[i] != my_string[i])
+            return 0;
     }
-    
-
-
-    return isPrefix ? 1 : 0;
+    return answer;
 }
