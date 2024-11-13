0d6eedd7ca4a11b3ce1fb8782269a6331ba574a9
이진우 <142082239+leejinwoo26@users.noreply.github.com>
2024-11-13 15:53:02 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 
 string solution(string s) {
-  bool newWord = true;
+   bool first = true;
 
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i] == ' ') {
-            newWord = true;
+for (int i = 0; i < s.size(); i++)
+{
+    if (s[i] == ' ') {
+        // 공백일 경우 다음 문자를 단어의 첫 글자로 인식
+        first = true;
+    }
+    else if (first) {
+        // 단어의 첫 글자는 대문자로
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            s[i] -= 32;
         }
-        else if (newWord) {
-            // 단어의 첫 글자를 대문자로 변환
-            if (s[i] >= 'a' && s[i] <= 'z') {
-                s[i] -= 32;
-            }
-            newWord = false;
-        }
-        else {
-            // 단어의 첫 글자가 아닐 때는 소문자로 변환
-            if (s[i] >= 'A' && s[i] <= 'Z') {
-                s[i] += 32;
-            }
+        first = false;
+    }
+    else {
+        // 첫 글자가 아니면 소문자로
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            s[i] += 32;
         }
     }
-
-    return s;
+}
+return s;
 }
